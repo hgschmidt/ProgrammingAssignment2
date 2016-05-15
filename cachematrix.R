629e16ca45620cdf748c9c5f1d4dda03d1a2277d
Hugo G Schmidt <HN@Hugos-MacBook-Pro.local>
2016-05-15 12:08:00 +0800

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,24 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(n = numeric){
+	print("Your Matrix is named 'd' ")
+	d <<- matrix(n *c(1, 3, 5, 6, 7, 1, 1, 2,9 ), nrow =3, ncol =3)  ## d now holds a SQUARE matrix (and only square matrices can have inverses) based on the numeric input above
 
-}
+	getmatrix <- function() d
+	id <<- NULL  ## id is being set to NULL to hold the eventual inverse
+	}
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <-function(x = matrix, ...) {
+	if(!is.null(id)){
+	print("retrieving inverse")	
+		return(id)
+		}
+	
+	else
+	{ 
+	print ("Your Inverse is called 'id'")
+	id <<- solve(x)  ## id now holds the inverse of d
+ 	}
+}	
